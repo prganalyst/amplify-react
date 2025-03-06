19335da824849946432c8a6345283cd02bdd235d
digitaliser <dwaraka@stragify.com>
2025-03-06 23:42:47 +0000

    Data Personalization for logged in user

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 const client = generateClient<Schema>();
 
 function App() {
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
   
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
